e1294d7fa0107d93b47f0c311e3d02ed3144c745
Noah Hoffman <noah.hoffman@gmail.com>
2017-08-24 15:55:29 -0700

    use bioconductor/release_core2:R3.4.1_Bioc3.5 as base image

--- docker/setup.R
+++ docker/setup.R
@@ -1,11 +1,32 @@
-packages <- c("argparse", "lattice", "latticeExtra", "tidyr", "rmarkdown")
-install.packages(
-    packages,
-    repos="http://cran.us.r-project.org",
-    dependencies=TRUE,
-    Ncpus=2)
+library(parallel)
+ncores <- min(c(8, parallel::detectCores()))
 
-source("https://bioconductor.org/biocLite.R")
-biocLite(c("ShortRead", "devtools", "qrqc"), suppressUpdates=FALSE)
+cran_packages <- c(
+    "argparse",
+    "lattice",
+    "latticeExtra",
+    "rmarkdown",
+    "R.utils",
+    "tidyr"
+)
+
+install.packages(
+    cran_packages,
+    repos="http://cran.us.r-project.org",
+    Ncpus=ncores,
+    clean=TRUE)
+
+bioc_packages <- c(
+    "devtools",
+    "qrqc"
+)
+
+## source("https://bioconductor.org/biocLite.R")
+biocLite(
+    bioc_packages,
+    suppressUpdates=TRUE,
+    Ncpus=ncores,
+    clean=TRUE)
+
 dada2_version = 'd57ccf5e22c8e709bf625a033dfa25561cbd392f' ## version 1.4.1
 devtools::install_github("benjjneb/dada2", ref=dada2_version)
