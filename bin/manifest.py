c3158bd725f99e9e91ee86e888b85f0cc5b58d4d
Noah Hoffman <noah.hoffman@gmail.com>
2019-10-14 12:37:35 -0700

    read excel manifest

--- bin/manifest.py
+++ bin/manifest.py
@@ -11,13 +11,44 @@ into a single location for the pipeline. Parameters for each specimen
 are also specified here.
 
 Required fields for the manifest:
- * ...
+ * sampleid - a string found in the fastq file name uniquely identifying a specimen
+ * sample_name - a specimen label for display in outputs
+
+Optional fields:
+ * project - an identifier that can be used to group specimens in subsequent analyses
+ * batch - an identifer used to group specimens into PCR batches for
+   dada2 model generation
+ * controls - a column that can be used to indicate which specimens
+   are controls. Specimens matching --neg-control-pattern are defined
+   as negative controls
 """
 
 import os
 import sys
 import argparse
 import json
+from itertools import takewhile
+from operator import attrgetter
+
+import openpyxl
+
+KEEPCOLS = {'sampleid', 'sample_name', 'project', 'batch', 'controls'}
+
+
+def read_manifest(fname, keepcols=KEEPCOLS):
+    wb = openpyxl.load_workbook(fname)
+    sheet = wb[wb.sheetnames[0]]
+    valgetter = attrgetter('value')
+
+    rows = (r for r in sheet.iter_rows() if r[0].value)
+    header = next(rows)
+    fieldnames = list(map(valgetter, takewhile(valgetter, header)))
+    fieldnames = [(f if f in keepcols else '_') for f in fieldnames]
+
+    for row in rows:
+        d = dict(zip(fieldnames, map(valgetter, row)))
+        d.pop('_')
+        yield d
 
 
 def main(arguments):
@@ -30,8 +61,9 @@ def main(arguments):
     parser.add_argument('-o', '--outfile', help="Output .json file")
 
     args = parser.parse_args(arguments)
-
-    print(args)
+    manifest = read_manifest(args.manifest)
+    for d in manifest:
+        print(d)
 
 
 if __name__ == '__main__':
