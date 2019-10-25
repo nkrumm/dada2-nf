1a134cd7d1c480186937adbaa016611efd07137b
Noah Hoffman <noah.hoffman@gmail.com>
2019-10-24 22:19:18 -0700

    update dada2 to 1.12

--- docker/build.sh
+++ docker/build.sh
@@ -2,16 +2,18 @@
 
 set -e
 
-# if ! git diff-index --quiet HEAD; then
-#     echo "Error: git repo is dirty - commit and try again"
-#     echo
-#     git status
-#     exit 1
-# fi
+if ! git diff-index --quiet HEAD; then
+    echo "Error: git repo is dirty - commit and try again"
+    echo
+    git status
+    exit 1
+fi
 
-tag=${1-v1.8}
+tag=${1-v1.12}
+rev=$(git describe --tags --dirty)
 python3 get_tag.py $tag > /dev/null
 DADA2_COMMIT=$(python3 get_tag.py $tag)
 echo "building image dada2:$tag from commit $DADA2_COMMIT"
 
-time docker build --build-arg DADA2_COMMIT=$DADA2_COMMIT --rm --force-rm -t dada2:$tag .
+time docker build --build-arg DADA2_COMMIT=$DADA2_COMMIT \
+     --rm --force-rm -t dada2:${tag}-${rev} .
