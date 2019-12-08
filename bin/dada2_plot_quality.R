322ef152577dde19d3ef6883c97454a4823bfcb3
Noah Hoffman <noah.hoffman@gmail.com>
2019-12-08 13:17:49 -0800

    detect empty files

--- bin/dada2_plot_quality.R
+++ bin/dada2_plot_quality.R
@@ -1,8 +1,8 @@
 #!/usr/bin/env Rscript
 
 suppressWarnings(suppressMessages(library(argparse, quietly = TRUE)))
-suppressWarnings(suppressMessages(library(dada2, quietly = TRUE)))
-suppressWarnings(suppressMessages(library(ggplot2, quietly = TRUE)))
+## suppressWarnings(suppressMessages(library(dada2, quietly = TRUE)))
+## suppressWarnings(suppressMessages(library(ggplot2, quietly = TRUE)))
 
 na.ifnull <- function(val){
   if(is.null(val)){
@@ -10,6 +10,23 @@ na.ifnull <- function(val){
   }else{
     val
   }
+}
+
+## read.fq <- function(fname, hash.prop=1){
+##   temp <- tempfile(fileext=".fastq")
+##   system2('gunzip', c('-d', '--stdout', fname), stdout=temp)
+##   s.fastq <- qrqc::readSeqFile(temp, hash.prop=hash.prop)
+##   unlink(temp)
+##   s.fastq
+## }
+
+gzip_size <- function(fname){
+  ## TODO: this is liklely to be pretty fragile
+  ## stdout looks something like
+  ## [1] "         compressed        uncompressed  ratio uncompressed_name"
+  ## [2] "                 53                   0   0.0% filename
+  out <- system2('gunzip', c('-l', fname), stdout=TRUE)
+  as.integer(unlist(strsplit(out[2], "\\s+"))[3])
 }
 
 main <- function(arguments){
@@ -20,12 +37,20 @@ main <- function(arguments){
   parser$add_argument('-o', '--outfile', default='plot_quality.svg')
   ## parser$add_argument('--title', default='quality plot')
 
-  parser$add_argument('--nreads', type='double', default=10000)
+  parser$add_argument('--nreads', type='double', default=100000)
   parser$add_argument('--trim-left', type='double')
   parser$add_argument('--f-trunc', type='double')
   parser$add_argument('--r-trunc', type='double')
 
   args <- parser$parse_args(arguments)
+
+  if(gzip_size(args$r1) == 0){
+    svg(args$outfile, width=10, height=4)
+    plot.new()
+    title(gettextf('%s is empty', basename(args$r1)))
+    invisible(dev.off())
+    quit()
+  }
 
   p.r1 <- dada2::plotQualityProfile(args$r1, args$nreads)
   p.r2 <- dada2::plotQualityProfile(args$r2, args$nreads)
