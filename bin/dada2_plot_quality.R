9205e79df37f94f72deea10eafe6559abc9726ca
Noah Hoffman <noah.hoffman@gmail.com>
2019-12-08 13:43:06 -0800

    plots are png, add plot titles

--- bin/dada2_plot_quality.R
+++ bin/dada2_plot_quality.R
@@ -1,8 +1,8 @@
 #!/usr/bin/env Rscript
 
 suppressWarnings(suppressMessages(library(argparse, quietly = TRUE)))
-## suppressWarnings(suppressMessages(library(dada2, quietly = TRUE)))
-## suppressWarnings(suppressMessages(library(ggplot2, quietly = TRUE)))
+suppressWarnings(suppressMessages(library(dada2, quietly = TRUE)))
+suppressWarnings(suppressMessages(library(ggplot2, quietly = TRUE)))
 
 na.ifnull <- function(val){
   if(is.null(val)){
@@ -34,20 +34,26 @@ main <- function(arguments){
   parser <- ArgumentParser()
   parser$add_argument('r1', help='fastq.gz containing forward read')
   parser$add_argument('r2', help='fastq.gz containing reverse read')
-  parser$add_argument('-o', '--outfile', default='plot_quality.svg')
-  ## parser$add_argument('--title', default='quality plot')
+  parser$add_argument('-o', '--outfile', default='plot_quality.png')
+  parser$add_argument('--specimen', default='specimen',
+                      help='specimen identifier for title')
 
   parser$add_argument('--nreads', type='double', default=100000)
-  parser$add_argument('--trim-left', type='double')
+  parser$add_argument('--trim-left', type='double', default=1)
   parser$add_argument('--f-trunc', type='double')
   parser$add_argument('--r-trunc', type='double')
 
   args <- parser$parse_args(arguments)
 
+  trim_left <- na.ifnull(args$trim_left)
+  f_trunc <- na.ifnull(args$f_trunc)
+  r_trunc <- na.ifnull(args$r_trunc)
+
+  height = 480
   if(gzip_size(args$r1) == 0){
-    svg(args$outfile, width=10, height=4)
+    png(args$outfile, width=height * 2, height=height)
     plot.new()
-    title(gettextf('%s is empty', basename(args$r1)))
+    title(gettextf('%s is empty', args$specimen))
     invisible(dev.off())
     quit()
   }
@@ -56,20 +62,23 @@ main <- function(arguments){
   p.r2 <- dada2::plotQualityProfile(args$r2, args$nreads)
 
   ## mark positions at which reads will be trimmed
-  if(!is.null(args$trim_left)){
-    p.r1 <- p.r1 + geom_vline(
-                 xintercept=c(na.ifnull(args$trim_left), na.ifnull(args$f_trunc)))}
+  p.r1 <- p.r1 +
+    ggtitle(gettextf('%s R1: trim_left: %s  f_trunc: %s',
+                     args$specimen, trim_left, f_trunc)) +
+    geom_vline(xintercept=c(trim_left, f_trunc))
 
   if(!is.null(args$trim_left)){
-    p.r2 <- p.r2 + geom_vline(
-                 xintercept=c(na.ifnull(args$trim_left), na.ifnull(args$r_trunc)))}
+    p.r2 <- p.r2 +
+      ggtitle(gettextf('%s R2: trim_left: %s  r_trunc: %s',
+                       args$specimen, trim_left, r_trunc)) +
+      geom_vline(xintercept=c(trim_left, r_trunc))}
 
   fig <- gridExtra::grid.arrange(p.r1, p.r2, nrow=1)
 
   ## ggsave fonts are less pleasing by default...
   ## ggplot2::ggsave(args$outfile, fig, width=10, height=4, units="in")
 
-  svg(args$outfile, width=10, height=4)
+  png(args$outfile, width=height * 2, height=height)
   plot(fig)
   invisible(dev.off())
 }
